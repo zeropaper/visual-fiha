a8854a3bcfb4d201d466e36e0602e55c7fcc705b
Valentin Vago <zeropaper@irata.ch>
2016-12-24 13:47:27 +0100

    remove some dead files

--- canvas-worker.js
+++
@@ -1,7 +0,0 @@
-
-onmessage = function(e) {
-  console.log('Message received from main script', e.data);
-  var workerResult = 'Result: ' + (e.data[0] + e.data[1]);
-  console.log('Posting message back to main script');
-  postMessage(workerResult);
-}
