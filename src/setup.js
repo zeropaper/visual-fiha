f32d8891aa1458c845b4ab758c9efa735baff430
Valentin Vago <zeropaper@irata.ch>
2017-01-02 10:31:48 +0100

    work on setup audio layer

--- src/setup.js
+++ src/setup.js
@@ -268,27 +268,48 @@ var canvasLayers = [
       var rad = Math.PI * 2;
 
       var i = 0, a, f, td, lx, ly;
-      ctx.strokeStyle = 'red';
-      ctx.beginPath();
-      for (i = 0; i < bufferLength; i++) {
-        a = ((rad / bufferLength) * i) - Math.PI;
-        f = (r / 100) * (freqArray[i] / 2);
-        lx = Math.round(x + Math.cos(a) * f);
-        ly = Math.round(y + Math.sin(a) * f);
-        ctx.lineTo(lx, ly);
-      }
-      ctx.stroke();
+      var original = {
+        lineWidth: ctx.lineWidth,
+        lineCap: ctx.lineCap,
+        lineJoin: ctx.lineJoin,
+        strokeStyle: ctx.strokeStyle,
+      };
 
-      ctx.strokeStyle = 'azure';
-      ctx.beginPath();
-      for (i = 0; i < bufferLength; i++) {
-        a = ((rad / bufferLength) * i) - Math.PI;
-        td = (r / 100) * (timeDomainArray[i] / 2);
-        lx = Math.round(x + Math.cos(a) * td);
-        ly = Math.round(y + Math.sin(a) * td);
-        ctx.lineTo(lx, ly);
+      ctx.lineJoin = 'round';
+      ctx.lineCap = 'round';
+
+      ctx.strokeStyle = 'red';
+      var col;
+      for (var lw = y*2; lw >= y*0.2; lw-=y*0.2) {
+        col = col === 'white' ? 'black' : 'white';
+        ctx.strokeStyle = col;
+        ctx.lineWidth = lw;
+
+        // ctx.beginPath();
+        // for (i = 0; i < bufferLength; i++) {
+        //   a = ((rad / bufferLength) * i) - Math.PI;
+        //   f = (r / 100) * (freqArray[i] / 2);
+        //   lx = Math.round(x + Math.cos(a) * f);
+        //   ly = Math.round(y + Math.sin(a) * f);
+        //   ctx.lineTo(lx, ly);
+        // }
+        // ctx.stroke();
+
+        ctx.beginPath();
+        for (i = 0; i < bufferLength; i++) {
+          a = ((rad / bufferLength) * i) - Math.PI;
+          td = (r / 100) * (timeDomainArray[i] / 2);
+          lx = Math.round(x + Math.cos(a) * td);
+          ly = Math.round(y + Math.sin(a) * td);
+          ctx.lineTo(lx, ly);
+        }
+        ctx.stroke();
       }
-      ctx.stroke();
+
+      ctx.lineWidth = original.lineWidth;
+      ctx.lineCap = original.lineCap;
+      ctx.lineJoin = original.lineJoin;
+      ctx.strokeStyle = original.strokeStyle;
     }
   },
 
