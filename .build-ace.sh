7248c5123946b12c26b5248e631e3214991ef676
Valentin Vago <zeropaper@irata.ch>
2017-03-06 10:01:14 +0100

    update ace-build

--- .build-ace.sh
+++ .build-ace.sh
@@ -16,6 +16,7 @@ cat ./node_modules/ace-builds/src/mode-javascript.js >> ./ace-build.js
 cat ./node_modules/ace-builds/src/mode-svg.js >> ./ace-build.js
 cat ./node_modules/ace-builds/src/mode-yaml.js >> ./ace-build.js
 cat ./node_modules/ace-builds/src/snippets/text.js >> ./ace-build.js
+cat ./node_modules/ace-builds/src/snippets/css.js >> ./ace-build.js
 cat ./node_modules/ace-builds/src/snippets/javascript.js >> ./ace-build.js
 cat ./node_modules/ace-builds/src/snippets/yaml.js >> ./ace-build.js
 cat ./node_modules/ace-builds/src/theme-github.js >> ./ace-build.js
