606123ca39416c2873207eb9172693835aca8d3d
Valentin Vago <zeropaper@irata.ch>
2017-03-09 10:06:31 +0100

    add setProperty method and cleanup layer view

--- src/layer/state.js
+++ src/layer/state.js
@@ -8,94 +8,10 @@ var LayerState = State.extend({
     name: ['string', true, null],
     type: ['string', true, 'default'],
     active: ['boolean', true, true],
-
-    // backfaceVisibility: ['boolean', true, false],
-    mixBlendMode: {
-      type: 'string',
-      default: 'normal',
-      required: true,
-      values: [
-        'normal',
-        'multiply',
-        'screen',
-        'overlay',
-        'darken',
-        'lighten',
-        'color-dodge',
-        'color-burn',
-        'hard-light',
-        'soft-light',
-        'difference',
-        'exclusion',
-        'hue',
-        'saturation',
-        'color',
-      ]
-    },
     opacity: {
       type: 'number',
       default: 100
     },
-    // // perspective: {
-    // //   type: 'number',
-    // //   default: 0
-    // // },
-    // rotateX: {
-    //   type: 'number',
-    //   default: 0
-    // },
-    // rotateY: {
-    //   type: 'number',
-    //   default: 0
-    // },
-    // rotateZ: {
-    //   type: 'number',
-    //   default: 0
-    // },
-    // translateX: {
-    //   type: 'number',
-    //   default: 0
-    // },
-    // translateY: {
-    //   type: 'number',
-    //   default: 0
-    // },
-    // // // translateZ: {
-    // // //   type: 'number',
-    // // //   default: 0
-    // // // },
-    // scaleX: {
-    //   type: 'number',
-    //   default: 100
-    // },
-    // scaleY: {
-    //   type: 'number',
-    //   default: 100
-    // },
-    // // // scaleZ: {
-    // // //   type: 'number',
-    // // //   default: 1
-    // // // },
-    // // originX: {
-    // //   type: 'number',
-    // //   required: false,
-    // //   default: 0
-    // // },
-    // // originY: {
-    // //   type: 'number',
-    // //   required: false,
-    // //   default: 0
-    // // },
-    // skewX: {
-    //   type: 'number',
-    //   required: false,
-    //   default: 0
-    // },
-    // skewY: {
-    //   type: 'number',
-    //   required: false,
-    //   default: 0
-    // },
     zIndex: ['number', true, 0]
   },
 
@@ -119,5 +35,7 @@ var LayerState = State.extend({
     }
   }
 });
+
 LayerState.types = {};
+
 module.exports = LayerState;
