f1b8b2e54fb197815c170d16cb258e2aa22e401a
Valentin Vago <zeropaper@irata.ch>
2017-03-25 14:47:51 +0100

    change gh-page handling
    close #27

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,107 @@
 /*jshint node: true*/
 'use strict';
+var marked = require('marked');
+var mkdirp = require('mkdirp').sync;
+var fs = require('fs');
 var resolve = require('path').resolve;
+var dirname = require('path').dirname;
 var webpack = require('webpack');
 
+var srcPath = resolve(__dirname, 'src');
+var destPath = resolve(__dirname, 'docs');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var readFileSync = fs.readFileSync;
+var writeFileSync = fs.writeFileSync;
+
+
+
+function MarkItDownPlugin(options) {
+  this.files = options.files;
+  this.cwd = options.cwd;
+  this.dest = options.dest;
+}
+
+MarkItDownPlugin.prototype.apply = function(compiler) {
+  var files = this.files;
+  var cwd = this.cwd;
+  var dest = this.dest;
+
+  compiler.plugin('emit', function(compilation, callback) {
+
+    files.forEach(function (transformedFilePath) {
+      var src = readFileSync(resolve(cwd, transformedFilePath), 'utf8');
+
+      var out = '';
+      var startTag = 'marked-start';
+      var openTag;
+
+      src.split(/<!--\s*(marked-start\s+[^\s]+|marked-end)\s*-->/igm)
+        .forEach(function(piece) {
+          var filepath;
+
+          if (piece.indexOf('marked-start') === 0) {
+            openTag = true;
+            filepath = piece.slice(startTag.length).trim();
+            out = out + '<!-- marked-start ' + filepath + ' -->\n';
+            out = out + marked(readFileSync(resolve(cwd, filepath), 'utf8'));
+          }
+          else if (piece.indexOf('marked-end') === 0) {
+            out = out + '\n<!-- marked-end -->';
+            openTag = false;
+          }
+          else if (!openTag) {
+            out = out + piece;
+          }
+        });
+
+      var absPath = resolve(dest, transformedFilePath);
+      mkdirp(dirname(absPath));
+      writeFileSync(absPath, out);
+    });
+
+    callback();
+  });
+};
+
+
+function ConcatPlugin(options) {
+  this.files = options.files;
+  this.cwd = options.cwd;
+  this.dest = options.dest;
+}
+
+ConcatPlugin.prototype.apply = function(compiler) {
+  var files = this.files;
+  var cwd = this.cwd;
+  var dest = this.dest;
+
+  compiler.plugin('emit', function(compilation, callback) {
+    var concatenated = '';
+    console.info('creating %s', dest);
+    var filepath;
+    files.forEach(function (file) {
+      filepath = resolve(cwd, file);
+      console.info('  adding %s', filepath);
+
+      concatenated += readFileSync(filepath, 'utf8');
+    });
+
+    mkdirp(dirname(dest));
+    writeFileSync(dest, concatenated);
+
+    callback();
+  });
+};
+
+
+
 module.exports = function(env) {
+  var acePath = resolve(__dirname, 'node_modules/ace-builds/src');
+
   env = env || {};
   return {
-    context: resolve(__dirname, 'src'),
+    context: srcPath,
     entry: {
       'controller':'./controller-app.js',
       'screen':'./screen-app.js',
@@ -28,21 +123,66 @@ module.exports = function(env) {
     },
 
     output: {
+      path: destPath,
       filename: '[name]-build.js'
     },
-    // http://cheng.logdown.com/posts/2016/03/25/679045
-    // devtool: env.prod ? 'cheap-module-source-map' : 'cheap-module-eval-source-map',
+
     devtool: env.prod ? 'source-map' : 'inline-source-map',
 
     plugins: [
-      new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor-build.js'})
+      new MarkItDownPlugin({
+        cwd: srcPath,
+        dest: destPath,
+        files: [
+          'index.html'
+        ]
+      }),
+
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor-build.js'
+      }),
+
+      new CopyWebpackPlugin([
+        {from: resolve(__dirname, 'assets'), to: 'assets'},
+        {from: 'setup.js'},
+        {from: 'controller.html'},
+        {from: 'screen.html'},
+        {from: resolve(acePath, 'worker-javascript.js')},
+        {from: resolve(acePath, 'worker-css.js')}
+      ]),
+
+      new ConcatPlugin({
+        cwd: acePath,
+        dest: resolve(destPath, 'ace-build.js'),
+        files: [
+          'ace.js',
+          'ext-beautify.js',
+          'ext-error_marker.js',
+          'ext-language_tools.js',
+          'ext-searchbox.js',
+          'ext-settings_menu.js',
+          'ext-split.js',
+          'ext-statusbar.js',
+          'ext-whitespace.js',
+          'mode-css.js',
+          'mode-javascript.js',
+          'mode-svg.js',
+          'mode-yaml.js',
+          'snippets/text.js',
+          'snippets/css.js',
+          'snippets/javascript.js',
+          'snippets/yaml.js',
+          'theme-github.js',
+          'theme-monokai.js'
+        ]
+      })
     ],
 
     devServer: {
       clientLogLevel: 'error',
       host: 'localhost',
-      inline: true,
-      contentBase: __dirname
+      inline: true
     }
   };
 };
