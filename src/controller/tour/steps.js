9b32f88121b17d722cba76da26f5f0e0bcb0bdfa
Valentin Vago <zeropaper@irata.ch>
2017-03-31 11:26:27 +0200

    write tour of layers
    closes #29, #33

--- src/controller/tour/steps.js
+++ src/controller/tour/steps.js
@@ -5,7 +5,8 @@ module.exports = function(controllerView) {
       title: 'Screen layers',
       name: 'layers',
       selector: '.region-right .region-content',
-      text: 'The layers provide a way to compose an screen with different types of media.<br/>The aspect of a layer can be influenced by preset or custom variables.',
+      text: 'The layers provide a way to compose an screen with different types of media.<br/>'+
+        'The aspect of a layer can be influenced by preset or custom variables.',
       prepare: function() {
         controllerView.regionRight.focusTab('Layers');
       }
@@ -17,6 +18,47 @@ module.exports = function(controllerView) {
       text: 'You can add a layer here by giving it a name, choosing its type and clicking on the <span class="vfi-plus"></span> button.',
       prepare: function() {
         controllerView.regionRight.focusTab('Layers');
+      }
+    },
+    {
+      title: 'Layer details',
+      name: 'layer-details',
+      selector: 'section.layers>.items .svg-layer-control:nth-child(2) .layer-name',
+      text: 'Click on a layer name to open its details.',
+      prepare: function() {
+        controllerView.regionRight.focusTab('Layers');
+        controllerView.layersView.items.views[1]._showDetails();
+      }
+    },
+    {
+      title: 'Layer variables',
+      name: 'layer-variables',
+      selector: '.style-props',
+      text: 'The CSS variables defined here can then be used in the style editor.<br/>'+
+        'Try adding a variable name "<code>--rotation</code>" with its value "<code>calc(0.05deg * var(--frametime))</code>".<br/>'+
+        'The "<code>--frametime</code>" variable is a "screen" variable (and is available to all layers).',
+      prepare: function() {
+        controllerView.regionRight.focusTab('Layers');
+        setTimeout(function() {
+          controllerView.layersView.items.views[1]._showDetails();
+        }, 100);
+      }
+    },
+    {
+      title: 'Layer styles',
+      name: 'layer-styles',
+      selector: 'section.layers>.items .svg-layer-control:nth-child(2) .edit-css',
+      text: 'Each layer can be styled with CSS. To do so, click the <span class="vfi-code"></span> button in the layers tab.<br/>'+
+        'Try adding<br/>'+
+        ' "<code>transform: rotate(var(--rotation));</code>" between the brakets.',
+      prepare: function() {
+        var tour = this;
+        controllerView.regionRight.focusTab('Layers');
+        setTimeout(function() {
+          controllerView.layersView.items.views[1]._editLayerStyles();
+          var editorEl = document.querySelector('.region-right .region-content');
+          tour.setPosition(editorEl).blinkFocused(editorEl);
+        }, 2000);
       }
     },
     {
