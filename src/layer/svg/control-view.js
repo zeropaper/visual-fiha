25b8f228071955869ac211ca0dde281d5b24f8a6
Valentin Vago <zeropaper@irata.ch>
2017-04-14 09:59:41 +0200

    work on layers UI
    related to #55

--- src/layer/svg/control-view.js
+++ src/layer/svg/control-view.js
@@ -7,10 +7,11 @@ module.exports = ScreenLayerControlView.types.SVG = ScreenLayerControlView.exten
   template: `
     <section class="svg-layer-control">
       <header class="columns">
-        <div class="column no-grow gutter-right"><button class="active prop-toggle"></button></div>
-        <div class="column no-grow gutter-horizontal"><button title="Edit layer CSS" class="edit-css vfi-code"></button></div>
-        <div class="column no-grow gutter-horizontal"><button title="Edit SVG elements CSS" class="edit-svg-css vfi-cog-alt"></button></div>
-        <h3 class="column layer-name gutter-left" data-hook="name"></h3>
+        <div class="column no-grow"><button class="active prop-toggle"></button></div>
+        <div class="column no-grow"><button title="Edit layer CSS" class="edit-css vfi-code"></button></div>
+        <h5 class="column no-grow layer-type"><span data-hook="type"></span></h5>
+        <div class="column no-grow"><button title="Edit SVG elements CSS" class="edit-svg-css vfi-cog-alt"></button></div>
+        <h3 class="column layer-name gutter-horizontal" data-hook="name"></h3>
         <div class="column no-grow text-right"><button class="vfi-trash-empty remove-layer"></button></div>
       </header>
 
