356b79e07347a6899f25b1e759aa98e2881e5033
Valentin Vago <zeropaper@irata.ch>
2017-04-29 17:04:33 +0200

    remove typeSafe paramter derived property

--- src/parameter/state.js
+++ src/parameter/state.js
@@ -22,15 +22,6 @@ var ParamState = State.extend({
       fn: function() {
         return objectPath(this);
       }
-    },
-    typeSafe: {
-      deps: ['value', 'type'],
-      fn: function() {
-        if (this.type === 'boolean') return !!this.value;
-        if (this.type === 'string') return (this.value || '').toString();
-        if (this.type === 'number') return Number(this.value || 0);
-        return this.value;
-      }
     }
   }
 });
