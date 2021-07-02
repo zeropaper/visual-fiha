c44debf87e1e46fc7a3be13caa5f724a05e63f22
Valentin Vago <zeropaper@irata.ch>
2021-07-02 13:38:55 +0200

    add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,19 @@
 /** @type {import('eslint').Linter.Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  extends: ['airbnb-typescript'],
+  plugins: ['jest'],
+  extends: ['airbnb-typescript', 'plugin:jest/recommended'],
   parserOptions: {
     project: './tsconfig.eslint.json',
   },
+  env: {
+    'jest/globals': true,
+  },
+  // rules: {
+  //   'jest/no-disabled-tests': 'warn',
+  //   'jest/no-focused-tests': 'error',
+  //   'jest/no-identical-title': 'error',
+  //   'jest/prefer-to-have-length': 'warn',
+  //   'jest/valid-expect': 'error',u
+  // },
 };
