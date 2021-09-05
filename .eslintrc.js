92bb6ba698f36f1a220c7d4bce45bf14d646ddc2
Valentin Vago <zeropaper@irata.ch>
2021-09-05 09:07:38 +0200

    fix(lint): fix dependencies for eslint airbnb base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,86 @@
 /** @type {import('eslint').Linter.Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  plugins: ['jest'],
-  extends: ['airbnb-typescript', 'plugin:jest/recommended'],
+  plugins: [
+    'import',
+  ],
+  extends: [
+    'airbnb-typescript',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
   parserOptions: {
     project: './tsconfig.eslint.json',
   },
-  env: {
-    'jest/globals': true,
-  },
   rules: {
-    'jest/no-disabled-tests': 'warn',
-    'jest/no-focused-tests': 'error',
-    'jest/no-identical-title': 'error',
-    'jest/prefer-to-have-length': 'warn',
-    'jest/valid-expect': 'error',
-
     'max-len': 'warn',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
 
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true, optionalDependencies: false, peerDependencies: false }],
+    'import/no-unresolved': ['error', { ignore: ['^vscode$'] }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
   },
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.test.tsx'],
+      plugins: [
+        'react',
+        'jest',
+        'jsx-a11y',
+      ],
+      extends: [
+        'airbnb-typescript',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'plugin:jest/recommended',
+      ],
+      env: {
+        'jest/globals': true,
+      },
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: true,
+            optionalDependencies: false,
+            peerDependencies: false,
+          },
+        ],
+      },
+    },
+    {
+      files: ['*.tsx'],
+      plugins: [
+        'react',
+        'jsx-a11y',
+      ],
+      extends: [
+        'airbnb-typescript',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+      ],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: true,
+            optionalDependencies: false,
+            peerDependencies: false,
+          },
+        ],
+      },
     },
   ],
 };
