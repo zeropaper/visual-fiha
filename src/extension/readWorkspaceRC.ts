aa0eed5bc78a4f16887c05f98dd4fa6cd41e884d
Valentin Vago <zeropaper@irata.ch>
2021-09-11 15:49:44 +0200

    feat(config): allow human readable JSON5

--- src/extension/readWorkspaceRC.ts
+++ src/extension/readWorkspaceRC.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
+import * as JSON5 from 'json5';
 import { FihaRC } from '../types';
 import getWorkspaceFolder from './getWorkspaceFolder';
 import asyncReadFile from './asyncReadFile';
 
 export default async function readWorkspaceRC(folderIndex = 0): Promise<FihaRC> {
   const folder = getWorkspaceFolder(folderIndex);
+
   const filepath = vscode.Uri.joinPath(folder.uri, 'fiha.json').fsPath;
   const content = await asyncReadFile(filepath);
-  return JSON.parse(content);
+  return JSON5.parse(content);
 }
