eaf910062002b1cdad23818899e0055e61079e52
Valentin Vago <zeropaper@irata.ch>
2021-09-11 09:27:45 +0200

    test(extension): mock vscode

---
+++ __mocks__/vscode.ts
@@ -0,0 +1,23 @@
+let workspacePath = '/absolute/fictive';
+
+const workspace = {
+  workspaceFolders: ['fictive-a', 'fictive-b'],
+};
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export function __setWorkspace(absPath: string, info: { worksapceFolders: string[] }) {
+  workspace.workspaceFolders = info.worksapceFolders;
+  workspacePath = absPath;
+}
+
+export const Uri = {
+  parse: jest.fn(),
+  joinPath: jest.fn((_, filepath: string) => {
+    // console.warn('[vscode mock] Uri.joinPath', workspacePath, filepath);
+    return {
+      fsPath: `${workspacePath}/${filepath}`,
+    };
+  }),
+};
+
+export { workspace };
---
+++ src/extension/readWorkspaceRC.test.ts
@@ -0,0 +1,6 @@
+// import readWorkspaceRC from './readWorkspaceRC';
+
+describe('readWorkspaceRC', () => {
+  it.todo('can read from fiha.json');
+  it.todo('can read from package.json');
+});
