bd5d10299c163f8561dad2d7360b75c168cd1a0a
Valentin Vago <zeropaper@irata.ch>
2021-09-18 08:29:44 +0200

    chore: update commit linting
    - set scopes
    - allow fixup! when committing
    - forbid fixup! when pushing
    - add ts check to config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,24 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+// @ts-check
+
+/** @type {import("@commitlint/types").UserConfig} */
+module.exports = {
+  // Ideally, this should be true for commits and false for pushu
+  defaultIgnores: !!process.env.X_COMMITLINT_DEFAULT_IGNORES,
+  extends: ['@commitlint/config-conventional'],
+  // https://commitlint.js.org/#/reference-rules
+  rules: {
+    'scope-enum': [2, 'always', [
+      'deps',
+      'extension',
+      'webserver',
+      'display',
+      'displayworker',
+      'worker',
+      'audio',
+      'mmidi',
+      'controls',
+      'com',
+      'demo',
+    ]],
+  },
+};
