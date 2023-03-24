ef707a77136bc525c795ab9b86328dacc01cfda2
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2023-03-24 19:30:45 +0100

    chore(deps): update everything

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,86 +1,91 @@
-/** @type {import('eslint').Linter.Config} */
-// eslint-disable-next-line no-undef
 module.exports = {
-  plugins: [
-    'import',
-  ],
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
-    'airbnb-typescript',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
   ],
   parserOptions: {
-    project: './tsconfig.eslint.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json'
   },
-  rules: {
-    'max-len': 'warn',
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  plugins: [
+    'react'
+  ]
+  // rules: {
+  //   'max-len': 'warn',
+  //   'no-console': ['warn', { allow: ['warn', 'error'] }],
 
-    'import/no-unresolved': ['error', { ignore: ['^vscode$'] }],
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-        optionalDependencies: false,
-        peerDependencies: false,
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: ['*.test.tsx'],
-      plugins: [
-        'react',
-        'jest',
-        'jsx-a11y',
-      ],
-      extends: [
-        'airbnb-typescript',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-        'plugin:jest/recommended',
-      ],
-      env: {
-        'jest/globals': true,
-      },
-      rules: {
-        'jest/no-disabled-tests': 'warn',
-        'jest/no-focused-tests': 'error',
-        'jest/no-identical-title': 'error',
-        'jest/prefer-to-have-length': 'warn',
-        'jest/valid-expect': 'error',
+  //   'import/no-unresolved': ['error', { ignore: ['^vscode$'] }],
+  //   'import/no-extraneous-dependencies': [
+  //     'error',
+  //     {
+  //       devDependencies: true,
+  //       optionalDependencies: false,
+  //       peerDependencies: false,
+  //     },
+  //   ],
+  // },
+  // overrides: [
+  //   {
+  //     files: ['*.test.tsx'],
+  //     plugins: [
+  //       'react',
+  //       'jest',
+  //       'jsx-a11y',
+  //     ],
+  //     extends: [
+  //       'airbnb-typescript',
+  //       'plugin:import/recommended',
+  //       'plugin:import/typescript',
+  //       'plugin:jest/recommended',
+  //     ],
+  //     env: {
+  //       'jest/globals': true,
+  //     },
+  //     rules: {
+  //       'jest/no-disabled-tests': 'warn',
+  //       'jest/no-focused-tests': 'error',
+  //       'jest/no-identical-title': 'error',
+  //       'jest/prefer-to-have-length': 'warn',
+  //       'jest/valid-expect': 'error',
 
-        'import/no-extraneous-dependencies': [
-          'error',
-          {
-            devDependencies: true,
-            optionalDependencies: false,
-            peerDependencies: false,
-          },
-        ],
-      },
-    },
-    {
-      files: ['*.tsx'],
-      plugins: [
-        'react',
-        'jsx-a11y',
-      ],
-      extends: [
-        'airbnb-typescript',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-      ],
-      rules: {
-        'import/no-extraneous-dependencies': [
-          'error',
-          {
-            devDependencies: true,
-            optionalDependencies: false,
-            peerDependencies: false,
-          },
-        ],
-      },
-    },
-  ],
-};
+  //       'import/no-extraneous-dependencies': [
+  //         'error',
+  //         {
+  //           devDependencies: true,
+  //           optionalDependencies: false,
+  //           peerDependencies: false,
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     files: ['*.tsx'],
+  //     plugins: [
+  //       'react',
+  //       'jsx-a11y',
+  //     ],
+  //     extends: [
+  //       'airbnb-typescript',
+  //       'plugin:import/recommended',
+  //       'plugin:import/typescript',
+  //     ],
+  //     rules: {
+  //       'import/no-extraneous-dependencies': [
+  //         'error',
+  //         {
+  //           devDependencies: true,
+  //           optionalDependencies: false,
+  //           peerDependencies: false,
+  //         },
+  //       ],
+  //     },
+  //   },
+  // ],
+}
