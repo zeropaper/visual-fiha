88d0158413a8a4a8783b45fb33881095d6b6988e
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2025-06-08 18:54:14 +0200

    feat: add broadcastChannel to ControlsContext

--- src/controls/ControlsContext.tsx
+++ src/controls/ControlsContext.tsx
@@ -1,109 +1,78 @@
-import type { Prettify } from "../types";
+import type { Context, InputConfig, SignalConfig } from "../types";
 import createFastContext from "./createFastContext";
 
-export type AudioInputValue = Record<
-  string, // track number as string
-  Record<
-    string, // channel number as string
-    Record<"data" | "min" | "max" | "average" | "median" | (string & {}), any>
-  >
->;
+const broadcastChannel = new BroadcastChannel("vf-controls");
 
-export type AudioInputMode = "file" | "files" | "mic";
-
-export interface AudioInputConfig {
-  minDecibels: number;
-  maxDecibels: number;
-  smoothingTimeConstant: number;
-  fftSize: number;
-}
-
-export interface AudioInputStruct {
-  name: "Audio";
-  type: "audio";
-  mode: AudioInputMode;
-  value: AudioInputValue | null;
-  config: AudioInputConfig;
-}
-
-type Input =
-  | AudioInputStruct
-  | {
-      name: "Time";
-      type: "time";
-      value: number;
-      config: { type: "absolute" | "relative" };
-    }
-  | {
-      name: "BPM";
-      type: "bpm";
-      value: number;
-      config: { min: number; max: number; startAt: number };
-    }
-  | {
-      name: "MIDI";
-      type: "midi";
-      value: { type: "device"; name: string; values: Record<string, number> }[];
-      config?: Record<string, any>;
-    };
-
-type Signal = {
-  name: string;
-  type?: string;
-  value?: any;
-};
-
-type LayerBase = {
-  name: string;
-  active: boolean;
-  setup: string;
-  animation: string;
-};
-
-type Layer = Prettify<
-  (
-    | {
-        type: "canvas";
-      }
-    | {
-        type: "threejs";
-      }
-  ) &
-    LayerBase
->;
-
-type Context = {
-  inputs: Input[];
-  signals: Signal[];
-  layers: Layer[];
-};
-
-export const {
-  FastContextProvider: AppFastContextProvider,
-  useFastContextFields: useAppFastContextFields,
-} = createFastContext({
-  inputs: [
-    { name: "Time", type: "time", value: 0, config: { type: "absolute" } },
-    {
-      name: "BPM",
-      type: "bpm",
-      value: 120,
-      config: { min: 30, max: 300, startAt: 0 },
-    },
-    {
-      name: "Audio",
-      type: "audio",
-      value: null,
-      mode: "mic",
-      config: {
-        minDecibels: -120,
-        maxDecibels: 80,
-        smoothingTimeConstant: 0.85,
-        fftSize: 1024,
+const { FastContextProvider: AppFastContextProvider, useFastContextFields } =
+  createFastContext({
+    inputs: [
+      { name: "Time", type: "time", value: 0, config: { type: "absolute" } },
+      {
+        name: "BPM",
+        type: "bpm",
+        config: { min: 30, max: 300, startAt: 0 },
       },
+      {
+        name: "Audio",
+        type: "audio",
+        mode: "mic",
+        config: {
+          minDecibels: -120,
+          maxDecibels: 80,
+          smoothingTimeConstant: 0.85,
+          fftSize: 1024,
+        },
+      },
+      { name: "MIDI", type: "midi", config: {} },
+    ] as InputConfig[],
+    signals: [] as SignalConfig[],
+    layers: [
+      {
+        id: "default-canvas",
+        active: true,
+        setup: "/* Canvas setup code */",
+        animation: "/* Canvas animation code */",
+        type: "canvas",
+      },
+      {
+        id: "default-threejs",
+        active: true,
+        setup: "/* Three.js setup code */",
+        animation: "/* Three.js animation code */",
+        type: "threejs",
+      },
+    ],
+    worker: {
+      setup: "/* Worker setup code */",
+      animation: "/* Worker animation code */",
     },
-    { name: "MIDI", type: "midi", value: [], config: {} },
-  ] as Input[],
-  signals: [],
-  layers: [],
-} satisfies Context);
+  } satisfies Context);
+
+broadcastChannel.addEventListener("message", (event) => {
+  console.info(
+    "[controls] BroadcastChannel incoming message:",
+    event.data,
+    event.source,
+  );
+});
+
+export function useAppFastContextFields<T extends keyof Context>(fields: T[]) {
+  const original = useFastContextFields(fields);
+  type OG = typeof original;
+  return Object.entries(original).reduce((acc, [key, value]) => {
+    acc[key as T] = {
+      get: (value as OG[keyof OG]).get,
+      set: (val) => {
+        (value as OG[keyof OG]).set(val);
+        broadcastChannel.postMessage({
+          type: "update",
+          field: key,
+          value: (value as OG[keyof OG]).get,
+        });
+      },
+    } as OG[T];
+    return acc;
+  }, {} as OG);
+}
+
+export { AppFastContextProvider, broadcastChannel };
