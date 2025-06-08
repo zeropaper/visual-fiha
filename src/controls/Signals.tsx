303d417e12599fa9c7b65de57372659a9bca5c35
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2025-06-08 18:53:26 +0200

    feat(controls): add Signals base component

---
+++ src/controls/Signals.tsx
@@ -0,0 +1,18 @@
+import { useAppFastContextFields } from "./ControlsContext";
+
+export function Signals() {
+  const {
+    signals: { get: signals, set: setSignals },
+  } = useAppFastContextFields(["signals"]);
+
+  return (
+    <details open>
+      <summary>Signals</summary>
+      <ul id="signals">
+        {signals.map((signal) => (
+          <li key={signal.name}>{signal.name}</li>
+        ))}
+      </ul>
+    </details>
+  );
+}
