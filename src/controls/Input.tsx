b4d7aa63640aeee366835fbd320c8acb3418f566
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2025-06-11 08:25:15 +0200

    feat(controls): add Input component

---
+++ src/controls/Input.tsx
@@ -0,0 +1,9 @@
+import { forwardRef } from "react";
+import styles from "./Input.module.css";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  return <input ref={ref} className={styles.input} type="text" {...props} />;
+});
+Input.displayName = "Input";
