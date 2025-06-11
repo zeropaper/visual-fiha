d62f16b3e3421f9838f6042c738196e20e297a8d
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2025-06-11 23:14:11 +0200

    fix: update GLTFLoader import path

--- src/layers/ThreeJS/ThreeJSLayer.ts
+++ src/layers/ThreeJS/ThreeJSLayer.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 import * as mathTools from "../../utils/mathTools";
 import miscTools from "../../utils/miscTools";
