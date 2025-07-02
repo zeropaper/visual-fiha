64453ecdf1f2843936b9a721eb399ca7027bbd87
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2025-07-02 19:26:41 +0200

    chore(controls): move the audio setup provider

--- src/controls/ControlsApp.tsx
+++ src/controls/ControlsApp.tsx
@@ -30,30 +30,30 @@ export default function ControlsApp() {
   return (
     <AppFastContextProvider>
       <FileSystemProvider>
-        <header className={styles.header}>
-          <div className={styles.branding}>
-            <svg
-              className={styles.logo}
-              xmlns="http://www.w3.org/2000/svg"
-              version="1.1"
-              viewBox="0 0 1000 1000"
-              height="32"
-              width="32"
-            >
-              <title>Visual Fiha logo</title>
-              <path
-                d="m 42.144772,100.70423 460.738378,798.02231 137.92084,-238.11891 -100.83949,0 -83.42113,-144.48961 267.68175,0 78.38889,-135.77355 -424.45952,0 -83.42215,-144.49139 591.30382,0 78.02825,-135.14885 z"
-                fill="currentColor"
-              />
-            </svg>
+        <AudioSetupProvider>
+          <header className={styles.header}>
+            <div className={styles.branding}>
+              <svg
+                className={styles.logo}
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                viewBox="0 0 1000 1000"
+                height="32"
+                width="32"
+              >
+                <title>Visual Fiha logo</title>
+                <path
+                  d="m 42.144772,100.70423 460.738378,798.02231 137.92084,-238.11891 -100.83949,0 -83.42113,-144.48961 267.68175,0 78.38889,-135.77355 -424.45952,0 -83.42215,-144.49139 591.30382,0 78.02825,-135.14885 z"
+                  fill="currentColor"
+                />
+              </svg>
 
-            <h1>Visual Fiha</h1>
-          </div>
-          <Menu />
-        </header>
-        <div className={styles.app}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <AudioSetupProvider>
+              <h1>Visual Fiha</h1>
+            </div>
+            <Menu />
+          </header>
+          <div className={styles.app}>
+            <Suspense fallback={<div>Loading...</div>}>
               <div className={styles.sidebar}>
                 <ControlDisplay />
                 <div className={styles.sidebarScrollable}>
@@ -83,9 +83,9 @@ export default function ControlsApp() {
               </div>
 
               <Timeline className={styles.timeline} />
-            </AudioSetupProvider>
-          </Suspense>
-        </div>
+            </Suspense>
+          </div>
+        </AudioSetupProvider>
       </FileSystemProvider>
     </AppFastContextProvider>
   );
