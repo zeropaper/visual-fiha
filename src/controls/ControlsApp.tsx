4d40a5fbd4093defe073b7e1e4f6a2f37240d1f8
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2025-10-11 09:47:23 +0200

    feat: replace SVG logo with VFLogo component

--- src/controls/ControlsApp.tsx
+++ src/controls/ControlsApp.tsx
@@ -1,5 +1,6 @@
 import { LoadingFallback } from "@ui/LoadingFallback";
 import { SidebarTabs } from "@ui/SidebarTabs";
+import { VFLogo } from "@ui/VFLogo";
 import { useIsMobile } from "@utils/useIsMobile";
 import {
   AudioLinesIcon,
@@ -116,20 +117,7 @@ export default function ControlsApp() {
         >
           <header className={styles.header}>
             <div className={styles.branding}>
-              <svg
-                className={styles.logo}
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                viewBox="0 0 1000 1000"
-                height="32"
-                width="32"
-              >
-                <title>Visual Fiha logo</title>
-                <path
-                  d="m 42.144772,100.70423 460.738378,798.02231 137.92084,-238.11891 -100.83949,0 -83.42113,-144.48961 267.68175,0 78.38889,-135.77355 -424.45952,0 -83.42215,-144.49139 591.30382,0 78.02825,-135.14885 z"
-                  fill="currentColor"
-                />
-              </svg>
+              <VFLogo className={styles.logo} width={32} height={32} />
 
               <h1>Visual Fiha</h1>
             </div>
