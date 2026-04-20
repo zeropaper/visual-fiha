b1e0f8c05ade4d3772cad5572c99a364ea7b6af5
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2026-04-20 17:54:06 +0200

    feat(controls): add styling for displays list and items

--- src/controls/features/Displays/Displays.tsx
+++ src/controls/features/Displays/Displays.tsx
@@ -1,5 +1,6 @@
 import { useAppFastContextFields } from "@contexts/ControlsContext";
 import { Button } from "@ui/Button";
+import styles from "./Displays.module.css";
 
 export function Displays() {
   const {
@@ -15,9 +16,17 @@ export function Displays() {
       >
         New Display
       </Button>
-      <ul id="displays">
+
+      <ul id="displays" className={styles.list}>
         {displays.map((display) => (
-          <li key={display.id}>{display.id}</li>
+          <li key={display.id} className={styles.item}>
+            <div className={styles.info}>
+              <span className={styles.bold}>{`${display.id}`}</span>
+              <span
+                className={styles.dimmed}
+              >{`${display.width}x${display.height}`}</span>
+            </div>
+          </li>
         ))}
       </ul>
     </>
