1a401308d46cee9b5ab090cea8eb0e7b5548de5a
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2026-05-21 12:44:30 +0200

    refactor: update Vitest configuration and dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@
 
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import { defineConfig } from "vitest/config";
 
 const dirname =
@@ -29,29 +28,6 @@ export default defineConfig({
         test: {
           include: ["./src/**/*.test.{ts,tsx,js,jsx}"],
           environment: "node",
-        },
-      },
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, ".storybook"),
-          }),
-        ],
-        test: {
-          name: "storybook",
-          browser: {
-            enabled: true,
-            headless: true,
-            instances: [
-              {
-                browser: "chromium",
-              },
-            ],
-          },
-          setupFiles: [".storybook/vitest.setup.ts"],
         },
       },
     ],
