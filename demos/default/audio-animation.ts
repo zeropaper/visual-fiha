4a5e39b0850d3d89c29286c379b8d8058be25080
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2026-05-25 08:16:32 +0200

    feat(demo): update audio example

--- demos/default/audio-animation.ts
+++ demos/default/audio-animation.ts
@@ -1,18 +1,35 @@
 clear();
-const keys = Object.keys(read("audio", {}));
-keys.forEach((key, i) => {
-  plot({
-    data: read(`audio.${key}.0.timeDomain.data` as any, []).map(
-      (v: number) => v - 127,
-    ),
-    color: hsla((1 / keys.length) * i, 1, 0.5, 0.5),
-    top: 0,
-    bottom: height(),
-    left: 0,
-    right: width(),
-    min: -100,
-    max: 100,
-    // legend: 'center',
-    // fontSize: vmin(fs)
+const now = read("time.elapsed", 0);
+
+const fns = [
+  () => 0,
+  () => {
+    const fAvg = read("audio.0.0.frequency.average", 90) - 90;
+    return fAvg * (1 / 90) * height(2);
+  },
+  () => {
+    const fAvg = read("audio.0.0.frequency.median", 90) - 90;
+    return fAvg * (1 / 90) * height(2);
+  },
+  () => {
+    const fAvg = read("audio.0.0.timeDomain.average", 127) - 127;
+    return fAvg * (1 / 90) * height(2);
+  },
+  () => {
+    const fAvg = read("audio.0.0.timeDomain.median", 127) - 127;
+    return fAvg * (1 / 90) * height(2);
+  },
+];
+
+const xGap = width(fns.length);
+const xBase = xGap * 0.5;
+
+const yBase = height(2);
+
+fns.forEach((fn, f) => {
+  circle({
+    y: yBase + fn(),
+    x: xGap * f + xBase,
+    fill: "lime",
   });
 });
